8ae60fdb9adef06b440d90e8627d36854441e6b5
Tomasz Kawalec <tkawalec369@gmail.com>
2021-09-18 11:04:32 +0200

    Update example.c

--- example.c
+++ example.c
@@ -5,10 +5,22 @@
 
 // TODO: make example use all functions from frames.h
 int main(int argc, char const* argv[]) {
-    printf("hello world\n");
+    printf("Hello world!\n\n");
 
-    uint8_t f[FRAME_LEN] = {'L', 'D', '2', '+', 'O', 'K', '#', 'C'};
+    uint8_t f[FRAME_LEN + 1] = "LD\x02+OK#C\0";  // Wrong CRC!
+    printf("Frame: \"%s\"\n", f);
     bool valid = frames_verify(f, FRAME_LEN);
-    printf("valid: %d\n", valid);
+    printf("Valid: %d\n\n", valid);
+
+    frames_create(f, FRAME_LEN, (uint8_t*)"LD", (uint8_t*)"OK");
+    printf("Frame: \"%s\"\n", f);
+    valid = frames_verify(f, FRAME_LEN);
+    printf("Valid: %d\n", valid);
+    uint8_t data[3] = "\0\0\0";
+    frames_read_data(f, FRAME_LEN, data);
+    printf("Data: \"%s\"\n", data);
+    uint8_t len = frames_len_data(f);
+    printf("Data length: %d\n", len);
+
     return 0;
 }
