c26314590ec7b2bbdd2fb4b3dd0df9d86eaa6e90
Tomasz Kawalec <tkawalec369@gmail.com>
2021-09-18 11:22:00 +0200

    Add comments to example.c

--- example.c
+++ example.c
@@ -8,19 +8,22 @@ int main(int argc, char const* argv[]) {
     printf("Hello world!\n\n");
 
     uint8_t f[FRAME_LEN + 1] = "LD\x02+OK#C\0";  // Wrong CRC!
-    printf("Frame: \"%s\"\n", f);
-    bool valid = frames_verify(f, FRAME_LEN);
-    printf("Valid: %d\n\n", valid);
+    printf("Frame: \"%s\"\n", f);                // Print frame
+    bool valid = frames_verify(f, FRAME_LEN);  // Check if frame is valid - it's
+                                               // invalid because CRC is wrong
+    printf("Valid: %d\n\n", valid);            // Print if frame is valid
 
-    frames_create(f, FRAME_LEN, (uint8_t*)"LD", (uint8_t*)"OK");
-    printf("Frame: \"%s\"\n", f);
-    valid = frames_verify(f, FRAME_LEN);
-    printf("Valid: %d\n", valid);
-    uint8_t data[3] = "\0\0\0";
-    frames_read_data(f, FRAME_LEN, data);
-    printf("Data: \"%s\"\n", data);
-    uint8_t len = frames_len_data(f);
-    printf("Data length: %d\n", len);
+    frames_create(f, FRAME_LEN, (uint8_t*)"LD",
+                  (uint8_t*)"OK");        // Create frame
+    printf("Frame: \"%s\"\n", f);         // Print generated frame
+    valid = frames_verify(f, FRAME_LEN);  // Check if it is valid
+    printf("Valid: %d\n", valid);         // Print if frame is valid
+    uint8_t data[3] =
+        "\0\0\0";  // Buffer for data which will be extracted from frame
+    frames_read_data(f, FRAME_LEN, data);  // Extract data from frame
+    printf("Data: \"%s\"\n", data);        // Print this data
+    uint8_t len = frames_len_data(f);      // Get data's length
+    printf("Data length: %d\n", len);      // Print data's length
 
     return 0;
 }
