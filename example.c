93a7f8056e8298c70744523b8564a67992038849
Tomasz Kawalec <tkawalec369@gmail.com>
2021-10-02 12:33:58 +0200

    Allow digits in frame header, update example

--- example.c
+++ example.c
@@ -18,12 +18,24 @@ int main(int argc, char const* argv[]) {
     printf("Frame: \"%s\"\n", f);         // Print generated frame
     valid = frames_verify(f, FRAME_LEN);  // Check if it is valid
     printf("Valid: %d\n", valid);         // Print if frame is valid
-    uint8_t data[3] =
+    uint8_t data1[3] =
         "\0\0\0";  // Buffer for data which will be extracted from frame
-    frames_read_data(f, FRAME_LEN, data);  // Extract data from frame
-    printf("Data: \"%s\"\n", data);        // Print this data
-    uint8_t len = frames_len_data(f);      // Get data's length
-    printf("Data length: %d\n", len);      // Print data's length
+    frames_read_data(f, FRAME_LEN, data1);  // Extract data from frame
+    printf("Data: \"%s\"\n", data1);        // Print this data
+    uint8_t len = frames_len_data(f);       // Get data's length
+    printf("Data length: %d\n\n", len);     // Print data's length
+
+    frames_create(f, FRAME_LEN, (uint8_t*)"M1",
+                  (uint8_t*)"OK");        // Create frame
+    printf("Frame: \"%s\"\n", f);         // Print generated frame
+    valid = frames_verify(f, FRAME_LEN);  // Check if it is valid
+    printf("Valid: %d\n", valid);         // Print if frame is valid
+    uint8_t data2[3] =
+        "\0\0\0";  // Buffer for data which will be extracted from frame
+    frames_read_data(f, FRAME_LEN, data2);  // Extract data from frame
+    printf("Data: \"%s\"\n", data2);        // Print this data
+    len = frames_len_data(f);               // Get data's length
+    printf("Data length: %d\n", len);       // Print data's length
 
     return 0;
 }
